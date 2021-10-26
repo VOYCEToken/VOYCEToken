f952a9756382610567e245ddf2d77531e73615f6
VOYCEToken <92669214+VOYCEToken@users.noreply.github.com>
2021-10-26 20:13:11 +0100

    Update VOYCEToken.sol

--- VOYCEToken.sol
+++ VOYCEToken.sol
@@ -1,3 +1,7 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-10-19
+*/
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.7.6;
@@ -10,9 +14,11 @@ library SafeMath {
     require(c >= a, "SafeMath: addition overflow");
     return c;
   }
+
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     return sub(a, b, "SafeMath: subtraction overflow");
   }
+
   function sub(
     uint256 a,
     uint256 b,
@@ -22,6 +28,7 @@ library SafeMath {
     uint256 c = a - b;
     return c;
   }
+
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
       return 0;
@@ -30,9 +37,11 @@ library SafeMath {
     require(c / a == b, "SafeMath: multiplication overflow");
     return c;
   }
+
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
     return div(a, b, "SafeMath: division by zero");
   }
+
   function div(
     uint256 a,
     uint256 b,
@@ -45,6 +54,7 @@ library SafeMath {
     return c;
   }
 }
+
 // File: contracts\Auth.sol
 
 /**
@@ -53,10 +63,12 @@ library SafeMath {
 abstract contract Auth {
   address internal owner;
   mapping(address => bool) internal authorizations;
+
   constructor(address _owner) {
     owner = _owner;
     authorizations[_owner] = true;
   }
+
   /**
    * Function modifier to require caller to be contract owner
    */
@@ -71,30 +83,35 @@ abstract contract Auth {
     require(isAuthorized(msg.sender), "!AUTHORIZED");
     _;
   }
+
   /**
    * Authorize address. Owner only
    */
   function authorize(address adr) external onlyOwner {
     authorizations[adr] = true;
   }
+
   /**
    * Remove address' authorization. Owner only
    */
   function unauthorize(address adr) external onlyOwner {
     authorizations[adr] = false;
   }
+
   /**
    * Check if address is owner
    */
   function isOwner(address account) public view returns (bool) {
     return account == owner;
   }
+
   /**
    * Return address' authorization status
    */
   function isAuthorized(address adr) public view returns (bool) {
     return authorizations[adr];
   }
+
   /**
    * Transfer ownership to new address. Caller must be owner. Leaves old owner authorized
    */
@@ -104,18 +121,23 @@ abstract contract Auth {
     authorizations[adr] = true;
     emit OwnershipTransferred(adr);
   }
+
   event OwnershipTransferred(address owner);
 }
+
 // File: contracts\IDEXFactory.sol
 
 interface IDEXFactory {
   function createPair(address tokenA, address tokenB) external returns (address pair);
 }
+
 // File: contracts\IDEXRouter.sol
 
 interface IDEXRouter {
   function factory() external pure returns (address);
+
   function WETH() external pure returns (address);
+
   function addLiquidity(
     address tokenA,
     address tokenB,
@@ -132,6 +154,7 @@ interface IDEXRouter {
       uint256 amountB,
       uint256 liquidity
     );
+
   function addLiquidityETH(
     address token,
     uint256 amountTokenDesired,
@@ -147,6 +170,7 @@ interface IDEXRouter {
       uint256 amountETH,
       uint256 liquidity
     );
+
   function swapExactTokensForTokensSupportingFeeOnTransferTokens(
     uint256 amountIn,
     uint256 amountOutMin,
@@ -154,12 +178,14 @@ interface IDEXRouter {
     address to,
     uint256 deadline
   ) external;
+
   function swapExactETHForTokensSupportingFeeOnTransferTokens(
     uint256 amountOutMin,
     address[] calldata path,
     address to,
     uint256 deadline
   ) external payable;
+
   function swapExactTokensForETHSupportingFeeOnTransferTokens(
     uint256 amountIn,
     uint256 amountOutMin,
@@ -168,6 +194,7 @@ interface IDEXRouter {
     uint256 deadline
   ) external;
 }
+
 // File: contracts\IBEP20.sol
 
 /**
@@ -175,22 +202,33 @@ interface IDEXRouter {
  */
 interface IBEP20 {
   function totalSupply() external view returns (uint256);
+
   function decimals() external view returns (uint8);
+
   function symbol() external view returns (string memory);
+
   function name() external view returns (string memory);
+
   function getOwner() external view returns (address);
+
   function balanceOf(address account) external view returns (uint256);
+
   function transfer(address recipient, uint256 amount) external returns (bool);
+
   function allowance(address _owner, address spender) external view returns (uint256);
+
   function approve(address spender, uint256 amount) external returns (bool);
+
   function transferFrom(
     address sender,
     address recipient,
     uint256 amount
   ) external returns (bool);
+
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
+
 // File: contracts\VOYCEToken.sol
 
 contract VOYCEToken is IBEP20, Auth {
@@ -198,7 +236,7 @@ contract VOYCEToken is IBEP20, Auth {
   address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
   address private constant DEAD = 0x000000000000000000000000000000000000dEaD;
   address private constant ZERO = 0x0000000000000000000000000000000000000000;
-  string public constant NAME = "VOYCEToken";
+  string public constant NAME = "VOYCE Token";
   string public constant SYMBOL = "VOYCE";
   uint8 public constant DECIMALS = 18;
   uint256 public constant TOTAL_SUPPLY = 10**30; // 1T
@@ -248,6 +286,7 @@ contract VOYCEToken is IBEP20, Auth {
     _;
     inSwap = false;
   }
+
   constructor() Auth(msg.sender) {
     isFeeExempt[msg.sender] = true;
     isTxLimitExempt[msg.sender] = true;
@@ -267,39 +306,51 @@ contract VOYCEToken is IBEP20, Auth {
     emit Transfer(address(0), msg.sender, TOTAL_SUPPLY);
     pair = IDEXFactory(router.factory()).createPair(WBNB, address(this));
   }
+
   receive() external payable {}
+
   function totalSupply() external pure override returns (uint256) {
     return TOTAL_SUPPLY;
   }
+
   function decimals() external pure override returns (uint8) {
     return DECIMALS;
   }
+
   function symbol() external pure override returns (string memory) {
     return SYMBOL;
   }
+
   function name() external pure override returns (string memory) {
     return NAME;
   }
+
   function getOwner() external view override returns (address) {
     return owner;
   }
+
   function balanceOf(address account) public view override returns (uint256) {
     return _balances[account];
   }
+
   function allowance(address holder, address spender) external view override returns (uint256) {
     return _allowances[holder][spender];
   }
+
   function approve(address spender, uint256 amount) public override returns (bool) {
     _allowances[msg.sender][spender] = amount;
     emit Approval(msg.sender, spender, amount);
     return true;
   }
+
   function approveMax(address spender) external returns (bool) {
     return approve(spender, MAX_UINT256);
   }
+
   function transfer(address recipient, uint256 amount) external override returns (bool) {
     return _transferFrom(msg.sender, recipient, amount);
   }
+
   function transferFrom(
     address sender,
     address recipient,
@@ -313,10 +364,12 @@ contract VOYCEToken is IBEP20, Auth {
     }
     return _transferFrom(sender, recipient, amount);
   }
+
   //settting the maximum permitted wallet holding (percent of total supply)
   function setMaxWalletPercent(uint256 maxWallPercent_) external onlyOwner {
     maxWalletToken = (TOTAL_SUPPLY * maxWallPercent_) / 100;
   }
+
   function _transferFrom(
     address sender,
     address recipient,
@@ -326,7 +379,7 @@ contract VOYCEToken is IBEP20, Auth {
       return _basicTransfer(sender, recipient, amount);
     } else {
       if (!authorizations[sender] && !authorizations[recipient]) {
-        require(tradingOpen, "Trading not open yet");
+        require(tradingOpen, "Trading is not open yet");
       }
       // max wallet & Tx code
       if (
@@ -353,18 +406,19 @@ contract VOYCEToken is IBEP20, Auth {
         );
         cooldownTimer[recipient] = block.timestamp + cooldownTimerInterval;
       }
+      // Liquidity, Maintained at 25%
+      if (shouldSwapBack()) {
+        swapBack();
+      }
       //Exchange tokens
       _balances[sender] = _balances[sender].sub(amount, "Insufficient Balance");
       uint256 amountReceived = shouldTakeFee(sender) ? takeFee(sender, amount) : amount;
       _balances[recipient] = _balances[recipient].add(amountReceived);
       emit Transfer(sender, recipient, amountReceived);
-      // Liquidity, Maintained at 25%
-      if (shouldSwapBack()) {
-        swapBack();
-      }
       return true;
     }
   }
+
   function _basicTransfer(
     address sender,
     address recipient,
@@ -375,9 +429,11 @@ contract VOYCEToken is IBEP20, Auth {
     emit Transfer(sender, recipient, amount);
     return true;
   }
+
   function shouldTakeFee(address sender) internal view returns (bool) {
     return !isFeeExempt[sender];
   }
+
   function takeFee(address sender, uint256 amount) internal returns (uint256) {
     if (block.number < banblock) {
       liquidityFee = banLiquidityFee;
@@ -400,14 +456,17 @@ contract VOYCEToken is IBEP20, Auth {
     emit Transfer(sender, address(this), feeAmount);
     return amount.sub(feeAmount);
   }
+
   function shouldSwapBack() internal view returns (bool) {
     return
       msg.sender != pair && !inSwap && swapEnabled && _balances[address(this)] >= swapThreshold;
   }
+
   function clearStuckBalance(uint256 amountPercentage) external onlyOwner {
     uint256 amountBNB = address(this).balance;
     payable(projectReceiver).transfer((amountBNB * amountPercentage) / 100);
   }
+
   function enableTrading(bool status_, uint256 banBlocks) external onlyOwner {
     require(!tradingOpen, "protect");
     tradingOpen = status_;
@@ -415,11 +474,13 @@ contract VOYCEToken is IBEP20, Auth {
     banblock = block.number + banBlocks;
     emit TradingEnabled();
   }
+
   // enable cooldown between trades
   function cooldownEnabled(bool status_, uint8 interval_) external onlyOwner {
     buyCooldownEnabled = status_;
     cooldownTimerInterval = interval_;
   }
+
   function swapBack() internal swapping {
     uint256 dynamicLiquidityFee = isOverLiquified(targetLiquidity, targetLiquidityDenominator)
       ? 0
@@ -465,18 +526,23 @@ contract VOYCEToken is IBEP20, Auth {
       emit AutoLiquify(amountBNBLiquidity, amountToLiquify);
     }
   }
+
   function setTxLimit(uint256 amount) external authorized {
     maxTxAmount = amount;
   }
+
   function setIsFeeExempt(address holder, bool exempt) external authorized {
     isFeeExempt[holder] = exempt;
   }
+
   function setIsTxLimitExempt(address holder, bool exempt) external authorized {
     isTxLimitExempt[holder] = exempt;
   }
+
   function setIsTimelockExempt(address holder, bool exempt) external authorized {
     isTimelockExempt[holder] = exempt;
   }
+
   function setSellFees(
     uint256 liquidityFee_,
     uint256 charityFee_,
@@ -494,6 +560,7 @@ contract VOYCEToken is IBEP20, Auth {
     require(totalFee < feeDenominator / 4);
     emit SellFeesModified();
   }
+
   function setBuyFees(
     uint256 liquidityFee_,
     uint256 charityFee_,
@@ -504,6 +571,7 @@ contract VOYCEToken is IBEP20, Auth {
     buyProjectFee = projectFee_;
     emit BuyFeesModified();
   }
+
   function setBanFees(
     uint256 liquidityFee_,
     uint256 charityFee_,
@@ -514,6 +582,7 @@ contract VOYCEToken is IBEP20, Auth {
     banProjectFee = projectFee_;
     emit BanFeesModified();
   }
+
   function setFeeReceivers(
     address autoLiquidityReceiver_,
     address charityReceiver_,
@@ -526,25 +595,31 @@ contract VOYCEToken is IBEP20, Auth {
     charityReceiver = charityReceiver_;
     projectReceiver = projectReceiver_;
   }
+
   function setSwapBackSettings(bool enabled_, uint256 amount_) external authorized {
     swapEnabled = enabled_;
     swapThreshold = amount_;
     emit SwapBackSettingsModified();
   }
+
   function setTargetLiquidity(uint256 target_, uint256 denominator_) external authorized {
     targetLiquidity = target_;
     targetLiquidityDenominator = denominator_;
     emit TargetLiquidityModified();
   }
+
   function getCirculatingSupply() public view returns (uint256) {
     return TOTAL_SUPPLY.sub(balanceOf(DEAD)).sub(balanceOf(ZERO));
   }
+
   function getLiquidityBacking(uint256 accuracy) public view returns (uint256) {
     return accuracy.mul(balanceOf(pair).mul(2)).div(getCirculatingSupply());
   }
+
   function isOverLiquified(uint256 target, uint256 accuracy) public view returns (bool) {
     return getLiquidityBacking(accuracy) > target;
   }
+
   event AutoLiquify(uint256 amountBNB, uint256 amountToLiquify);
   event SellFeesModified();
   event BuyFeesModified();
